d1639893d2fae6ac5944f821086f482691b7f9e1
cigreja <cigreja@live.com>
2017-09-19 10:39:44 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>FRNStore</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo 'Hello FRNStore';
+        ?>
+    </body>
+</html>
